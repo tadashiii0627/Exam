29dd00c1436a09548acd0e68dbcd45acd81447eb
sss2434 <tky2302035@stu.o-hara.ac.jp>
2024-04-24 12:34:48 +0900

    beanファイル4つを新しく作りました

---
+++ src/bean/Subject.java
@@ -0,0 +1,36 @@
+package bean;
+import java.io.Serializable;
+public class Subject implements Serializable{
+	/**
+	 * 学校コード:String
+	 */
+	private String cd;
+	/**
+	 * 教員名:String
+	 */
+	private String name;
+	/**
+	 * School:School
+	 */
+	private School school;
+
+	public String getCd() {
+		return cd;
+	}
+	public void setCd(String cd) {
+		this.cd = cd;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public School getSchool(){
+		return school;
+	}
+	public void setSchool(School school){
+		this.school = school;
+	}
+}
