534820f2ef1d3fbe93126524847f284fce52991a
T sasaki <tky2302046@stu.o-hara.ac.jp>
2024-04-25 11:18:35 +0900

    訂正 フォワード先を(student_list.jsp)から(subject_list.jsp)に変更

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -59,6 +59,6 @@ public class SubjectListAction extends Action{
 		req.setAttribute("ent_year_set", entYearSet);
 
 		// JSPへフォワード 7
-		req.getRequestDispatcher("student_list.jsp").forward(req, res);
+		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
 	}
 	}
