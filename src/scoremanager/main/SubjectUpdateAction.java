6af7edfff0cff5593f586858a293e32728142890
sss2434 <tky2302035@stu.o-hara.ac.jp>
2024-04-25 12:21:32 +0900

    SubjctUpdateActionを作成しました。 まだ1か所エラーを吐いているところがあります

---
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -0,0 +1,27 @@
+package scoremanager.main;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectUpdateAction extends Action{
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)throws Exception{
+		//DBから科目の詳細を取得
+				HttpSession session = request.getSession();
+				Teacher teacher = (Teacher)session.getAttribute("user");
+
+				SubjectDao subjectdao = new SubjectDao();// 科目Daoを初期化
+
+				String cd = request.getParameter("cd");
+				//String schoolCd = teacher.getSchool().getCd();
+
+				Subject subject = subjectdao.get(cd, teacher.getSchool());
+		//JSPへフォワード
+		request.getRequestDispatcher("subject_update.jsp").forward(request,response);
+	}
+}
