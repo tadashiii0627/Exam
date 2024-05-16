3b576ea54211cf0dd2f7f40a3d23a1bfa05d8351
seyama0828 <stu2302032@stu.o-hara.ac.jp>
2024-05-16 10:53:43 +0900

    できてるかわかりません。

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,8 +1,13 @@
 package dao;
 
+import bean.School;
+import bean.Student;
+import bean.Subject;
+import bean.Test;
+
 public class TestDao extends Dao{
 	private String baseSql = "select * from test where =?";
 
-
+	public Test get(Student student, Subject subject, School school, int no) throws Exception{}
 
 }
