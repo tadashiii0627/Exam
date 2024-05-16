22549e7e1cb1746f5138ff16c2ba252ecf84c01b
seyama0828 <stu2302032@stu.o-hara.ac.jp>
2024-05-16 15:36:25 +0900

    外面のDAO

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,5 +1,9 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.List;
+
 import bean.School;
 import bean.Student;
 import bean.Subject;
@@ -9,5 +13,10 @@ public class TestDao extends Dao{
 	private String baseSql = "select * from test where =?";
 
 	public Test get(Student student, Subject subject, School school, int no) throws Exception{}
-
+	private List<Test> postFilter(ResultSet rSet, School school) throws Exception {}
+	public List<Test> filter(int entYear ,String classNum , Subject subject , int num , School school) throws Exception {}
+	public boolean save(List<Test> list) throws Exception {}
+	private boolean save(Test test , Connection connection)throws Exception {}
+	public boolean delete(List<Test> list)throws Exception {}
+	private boolean delete  (Test test , Connection connection)throws Exception {}
 }
